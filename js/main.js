277b0fc7b9487db3f9a3463eb132c7e555a51387
Ludovico Pinzari <ludovico.pinzari@gmail.com>
2019-07-10 14:17:12 +1000

    Ref: wrap functions in menu

--- js/main.js
+++ js/main.js
@@ -1,46 +1,11 @@
 
 
-$('#symbol1').on('change', updateSymbols);
-$('#symbol2').on('change', updateSymbols);
+$('#symbol1').on('change', menu.updateSymbols);
+$('#symbol2').on('change', menu.updateSymbols);
 $('#playgame').on('click', play);
 
 
-// function updateSymbols() {
-//
-//   let symbol1 = $('#symbol1').val();
-//   let icon1 = $('.icon')[0];
-//
-//   if (symbol1 === "cross") {
-//     icon1.innerHTML = '<i class="fas fa-times" aria-hidden="true"></i>';
-//   } else if(symbol1 === "circle") {
-//     icon1.innerHTML = '<i class="fas fa-circle" aria-hidden="true"></i>';
-//   } else if(symbol1 === "star") {
-//     icon1.innerHTML = '<i class="fas fa-star" aria-hidden="true"></i>';
-//   }
-//
-//   let symbol2 = $('#symbol2').val();
-//   let icon2 = $('.icon')[1];
-//
-//   if (symbol2 === "cross") {
-//     icon2.innerHTML = '<i class="fas fa-times" aria-hidden="true"></i>';
-//   } else if(symbol2 === "circle") {
-//     icon2.innerHTML = '<i class="fas fa-circle" aria-hidden="true"></i>';
-//   } else if(symbol2 === "star") {
-//     icon2.innerHTML = '<i class="fas fa-star" aria-hidden="true"></i>';
-//   }
-//
-// }
-//
-// function getSymbolClass(symbol) {
-//
-//   if (symbol === "cross") {
-//     return "fas fa-times";
-//   } else if (symbol === "circle") {
-//     return "fas fa-circle";
-//   } else if (symbol === "star") {
-//     return "fas fa-star";
-//   }
-// }
+
 
 
 function play() {
@@ -58,8 +23,8 @@ function play() {
     namePly2 = "Player2";
   }
 
-  const player1 = player(namePly1,getSymbolClass(symbol1));
-  const player2 = player(namePly2,getSymbolClass(symbol2));
+  const player1 = player(namePly1,menu.getSymbolClass(symbol1));
+  const player2 = player(namePly2,menu.getSymbolClass(symbol2));
 
   game.addPlayer(player1);
   game.addPlayer(player2);
