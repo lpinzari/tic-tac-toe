c8e40bb088df45859be3dd63d6bf6f340d0aadc1
Ludovico Pinzari <ludovico.pinzari@gmail.com>
2019-07-10 20:40:38 +1000

    Ref: wrap modal functions

--- js/main.js
+++ js/main.js
@@ -1,39 +1,10 @@
 
 
-// $('#symbol1').on('change', menu.updateSymbols);
-// $('#symbol2').on('change', menu.updateSymbols);
-// $('#playgame').on('click', play);
-//
-//
-// function play() {
-//
-//   let symbolPly1 = menu.getSymbolPly1();
-//   let symbolPly2 = menu.getSymbolPly2();
-//   let namePly1 = menu.getNamePly1();
-//   let namePly2 = menu.getNamePly2();
-//
-//   if (namePly1 === "") {
-//     namePly1 = "Player1";
-//   }
-//
-//   if (namePly2 === "") {
-//     namePly2 = "Player2";
-//   }
-//
-//   const player1 = player(namePly1,menu.getSymbolClass(symbolPly1));
-//   const player2 = player(namePly2,menu.getSymbolClass(symbolPly2));
-//
-//   game.addPlayer(player1);
-//   game.addPlayer(player2);
-//   game.setTurn(0);
-//
-//   menu.hide();
-//   menu.showPanel();
-//   menu.showGrid();
-//
-// }
-
 const gameController = {
+
+  /*
+   * FUNCTION ATTACHED TO THE MENU EVENT
+   */
 
   play: function() {
 
@@ -62,6 +33,10 @@ const gameController = {
     menu.showGrid();
 
   },
+
+  /*
+   * FUNCTIONS ATTACHED TO THE GRID EVENTS
+   */
 
   markCell: function(event) {
 
@@ -93,6 +68,27 @@ const gameController = {
 
   },
 
+  /*
+   * FUNCTIONS ATTACHED TO THE MODAL VICTORY EVENTS
+   */
+
+  startNewGame: function() {
+
+    modalVictory.hide();
+    location.reload();
+
+  },
+
+  exitGame: function() {
+
+    modalVictory.hide();
+
+  },
+
+  /*
+   * EVENT LISTENERS
+   */
+
   menuHandler: function() {
 
     $('#symbol1').on('change', menu.updateSymbols);
@@ -103,79 +99,30 @@ const gameController = {
 
   gridHandler: function() {
 
-    $('.grid').on('click', markCell);
-    $('.restart').on('click',restartGame);
+    $('.grid').on('click', this.markCell);
+    $('.restart').on('click',this.restartGame);
 
   },
+
+  modalVictoryHandler: function() {
+
+    $('#ok-Button').on('click',this.startNewGame);
+    $('#no-Button').on('click',this.exitGame);
+
+  },
+
+  /*
+   * INITIALIZATION 
+   */
 
   init: function() {
 
     this.menuHandler();
     this.gridHandler();
+    this.modalVictoryHandler();
 
   }
 
 };
 
 gameController.init();
-
-
-
-/********************************************
- *         Click Event Listener              *
- *********************************************/
-
-$('#ok-Button').on('click',startNewGame);
-$('#no-Button').on('click',exitGame);
-
-
-
-function markCell(event) {
-
-    // See if the element clicked is an empty cell
-    if (board.isEmptyCell(event)){
-
-        var cell = event.target;
-        let value = cell.innerText;
-
-        game.getPlayerTurn().addMove(value);
-
-        let symbol = game.getPlayerTurn().getSymbol();
-        board.renderCell(cell, symbol);
-
-        if (game.isOver()) {
-
-          modalVictory.displayResult(game.hasWinner());
-        } else {
-          game.switchTurn();
-        }
-
-     }
-
-}
-
-/**
-* @desc close the pop up screen and reload the page
-*/
-function startNewGame () {
-
-    modalVictory.hide();
-    location.reload();
-
-}
-
-/**
-* @desc close the pop up screen
-*/
-function exitGame () {
-
-    modalVictory.hide();
-
-}
-
-// reload the page
-function restartGame() {
-
-    location.reload();
-
-}
